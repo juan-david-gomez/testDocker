9d4523738181c8fd04c850554d3957e0565eb341
Juan David Gomez <judagoes@gmail.com>
2017-04-30 17:54:40 -0500

    Mejoras

--- sample-service/api/public/javascript/index.js
+++ sample-service/api/public/javascript/index.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function(){
     $('#react').click(function(){
-        $(this).append('<p> Dont Touch Me</p>');
+        // $(this).append('<p> Dont Touch Me</p>');
+        $.get('/employee',{},function (res) {
+        	var data = res.data;
+        	$("#employees").html(' ');
+        	data.forEach(function (item,i) {        		
+        		$("#employees").append('<li>'+item.FullName+'</li>');
+        	});
+        	console.log(res);
+        },'json');
     });
 });
